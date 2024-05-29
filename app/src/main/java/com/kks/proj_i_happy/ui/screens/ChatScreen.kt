c7904e91c160b663b4915be960f3cdb0d5c7b679
Kuneosu <kks92155784@gmail.com>
2024-05-29 14:07:28 +0900

    Chat Screen update, Make Search Screen

--- app/src/main/java/com/kks/proj_i_happy/ui/screens/ChatScreen.kt
+++ app/src/main/java/com/kks/proj_i_happy/ui/screens/ChatScreen.kt
@@ -1,22 +1,38 @@
 package com.kks.proj_i_happy.ui.screens
 
+import androidx.compose.foundation.Image
 import androidx.compose.foundation.background
+import androidx.compose.foundation.border
 import androidx.compose.foundation.layout.Arrangement
 import androidx.compose.foundation.layout.Box
 import androidx.compose.foundation.layout.Column
 import androidx.compose.foundation.layout.Row
+import androidx.compose.foundation.layout.Spacer
 import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.foundation.layout.fillMaxWidth
+import androidx.compose.foundation.layout.height
 import androidx.compose.foundation.layout.padding
+import androidx.compose.foundation.layout.size
 import androidx.compose.foundation.lazy.LazyColumn
 import androidx.compose.foundation.lazy.items
 import androidx.compose.foundation.lazy.rememberLazyListState
+import androidx.compose.foundation.shape.CircleShape
 import androidx.compose.foundation.shape.RoundedCornerShape
 import androidx.compose.foundation.text.KeyboardActions
 import androidx.compose.foundation.text.KeyboardOptions
+import androidx.compose.material.icons.Icons
+import androidx.compose.material.icons.filled.ArrowBack
+import androidx.compose.material.icons.filled.Search
 import androidx.compose.material3.Button
+import androidx.compose.material3.ButtonDefaults
+import androidx.compose.material3.ExperimentalMaterial3Api
+import androidx.compose.material3.Icon
+import androidx.compose.material3.IconButton
+import androidx.compose.material3.Scaffold
 import androidx.compose.material3.Text
 import androidx.compose.material3.TextField
+import androidx.compose.material3.TextFieldDefaults
+import androidx.compose.material3.TopAppBar
 import androidx.compose.runtime.Composable
 import androidx.compose.runtime.LaunchedEffect
 import androidx.compose.runtime.mutableStateListOf
@@ -25,26 +41,39 @@ import androidx.compose.runtime.remember
 import androidx.compose.runtime.rememberCoroutineScope
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.graphics.Brush
 import androidx.compose.ui.graphics.Color
+import androidx.compose.ui.graphics.painter.Painter
+import androidx.compose.ui.res.painterResource
+import androidx.compose.ui.text.font.FontWeight
 import androidx.compose.ui.text.input.ImeAction
+import androidx.compose.ui.text.style.TextAlign
 import androidx.compose.ui.unit.dp
+import androidx.compose.ui.unit.sp
 import androidx.navigation.NavController
-import com.kks.proj_i_happy.util.DefaultScreen
+import com.kks.proj_i_happy.R
 import kotlinx.coroutines.launch
+import java.text.SimpleDateFormat
+import java.util.Date
+import java.util.Locale
 
 @Composable
-fun ChatScreen(navController: NavController){
-    DefaultScreen(navController) {
-        ChatBody()
-    }
+fun ChatScreen(navController: NavController) {
+    ChatBody(navController)
 }
 
+@OptIn(ExperimentalMaterial3Api::class)
 @Composable
-fun ChatBody() {
+fun ChatBody(navController: NavController) {
     val messages = remember { mutableStateListOf<Pair<String, Boolean>>() }
     val (text, setText) = remember { mutableStateOf("") }
     val listState = rememberLazyListState()
     val coroutineScope = rememberCoroutineScope()
+
+    LaunchedEffect(Unit) {
+        // Welcome 메시지를 추가합니다.
+        messages.add(Pair("Welcome to Happy AI Chat!", false))
+    }
 
     LaunchedEffect(messages.size) {
         if (messages.isNotEmpty()) {
@@ -54,67 +83,185 @@ fun ChatBody() {
         }
     }
 
-    Column(
-        modifier = Modifier.fillMaxSize(),
-        verticalArrangement = Arrangement.SpaceBetween
-    ) {
-        LazyColumn(
-            modifier = Modifier.weight(1f),
-            state = listState,
-            reverseLayout = false // 위에서 아래로 정렬
-        ) {
-            items(messages) { (message, isUserMessage) ->
-                ChatMessage(message = message, isUserMessage = isUserMessage)
-            }
-        }
-        Row(
-            modifier = Modifier
-                .padding(8.dp)
-                .fillMaxWidth(),
-            verticalAlignment = Alignment.CenterVertically
-        ) {
-            TextField(
-                value = text,
-                onValueChange = setText,
-                modifier = Modifier.weight(1f),
-                placeholder = { Text("Enter message") },
-                keyboardOptions = KeyboardOptions.Default.copy(
-                    imeAction = ImeAction.Send
-                ),
-                keyboardActions = KeyboardActions(
-                    onSend = {
-                        if (text.isNotEmpty()) {
-                            messages.add(Pair("You: $text", true))
-                            messages.add(Pair("Bot: ${getResponse(text)}", false))
-                            setText("")
-                        }
-                    }
-                )
-            )
-            Button(
-                onClick = {
-                    if (text.isNotEmpty()) {
-                        messages.add(Pair("You: $text", true))
-                        messages.add(Pair("Bot: ${getResponse(text)}", false))
-                        setText("")
+    Scaffold(
+        topBar = {
+            TopAppBar(
+                title = {
+                    Text(
+                        "Happy AI",
+                        modifier = Modifier.fillMaxWidth(),
+                        textAlign = TextAlign.Center
+                    )
+                },
+                navigationIcon = {
+                    IconButton(onClick = { navController.popBackStack() }) {
+                        Icon(Icons.Filled.ArrowBack, contentDescription = "Back")
                     }
                 },
-                modifier = Modifier.padding(start = 8.dp)
+                actions = {
+                    IconButton(onClick = { /*TODO*/ }) {
+                        Icon(Icons.Filled.Search, contentDescription = "Search")
+                    }
+                }
+            )
+        }
+    ) {
+        // here insert down elevation
+
+        Column(
+            modifier = Modifier
+                .fillMaxSize()
+                .padding(it),
+            verticalArrangement = Arrangement.SpaceBetween
+        ) {
+            Box(
+                modifier = Modifier
+                    .fillMaxWidth()
+                    .height(4.dp)
+                    .background(
+                        brush = Brush.verticalGradient(
+                            colors = listOf(Color.LightGray, Color.Transparent),
+                            startY = 0f,
+                            endY = Float.POSITIVE_INFINITY
+                        )
+                    )
+            )
+            LazyColumn(
+                modifier = Modifier.weight(1f),
+                state = listState,
+                reverseLayout = false // 위에서 아래로 정렬
             ) {
-                Text("Send")
+                items(messages) { (message, isUserMessage) ->
+                    ChatMessage(message = message, isUserMessage = isUserMessage)
+                }
+            }
+
+            Box(
+                modifier = Modifier
+                    .fillMaxWidth()
+                    .height(1.dp)
+                    .background(
+                        brush = Brush.verticalGradient(
+                            colors = listOf(Color.Transparent, Color.LightGray),
+                            startY = 0f,
+                            endY = Float.POSITIVE_INFINITY
+                        )
+                    )
+            )
+
+            Row(
+                modifier = Modifier
+                    .padding(8.dp)
+                    .fillMaxWidth(),
+                verticalAlignment = Alignment.CenterVertically
+            ) {
+                TextField(
+                    value = text,
+                    onValueChange = setText,
+                    modifier = Modifier.weight(1f),
+                    placeholder = { Text("Enter message") },
+                    keyboardOptions = KeyboardOptions.Default.copy(
+                        imeAction = ImeAction.Send
+                    ),
+                    keyboardActions = KeyboardActions(
+                        onSend = {
+                            if (text.isNotEmpty()) {
+                                messages.add(Pair(text, true))
+                                messages.add(Pair(getResponse(text), false))
+                                setText("")
+                            }
+                        }
+                    ),
+                    colors = TextFieldDefaults.colors(
+                        focusedContainerColor = Color.White,
+                        unfocusedContainerColor = Color.White,
+                        focusedIndicatorColor = Color.Transparent,
+                        unfocusedIndicatorColor = Color.Transparent
+                    )
+                )
+                Button(
+                    onClick = {
+                        if (text.isNotEmpty()) {
+                            messages.add(Pair(text, true))
+                            messages.add(Pair(getResponse(text), false))
+                            setText("")
+                        }
+                    },
+                    modifier = Modifier.padding(start = 8.dp),
+                    colors = ButtonDefaults.buttonColors(
+                        containerColor = Color.Red
+                    )
+                ) {
+                    Text("Send")
+                }
             }
         }
     }
+
 }
 
 @Composable
 fun ChatMessage(message: String, isUserMessage: Boolean) {
-    Row(
+    val timestamp = getCurrentTime()
+    val name = if (isUserMessage) "You" else "Happy AI"
+    val profileImage: Painter? = if (isUserMessage) painterResource(id = R.drawable.ic_launcher_foreground) else null
+
+    Column(
         modifier = Modifier
             .fillMaxWidth()
             .padding(8.dp),
-        horizontalArrangement = if (isUserMessage) Arrangement.End else Arrangement.Start
+        horizontalAlignment = if (isUserMessage) Alignment.End else Alignment.Start
     ) {
+        if (isUserMessage) {
+            Row(
+                verticalAlignment = Alignment.CenterVertically,
+                horizontalArrangement = Arrangement.End
+            ) {
+                Text(
+                    text = timestamp,
+                    color = Color.Gray,
+                    fontSize = 12.sp,
+                    modifier = Modifier.padding(end = 4.dp)
+                )
+                Text(
+                    text = name,
+                    color = Color.Gray,
+                    fontSize = 14.sp,
+                    fontWeight = FontWeight.Bold,
+                    modifier = Modifier.padding(end = 4.dp)
+                )
+                profileImage?.let {
+                    Image(
+                        painter = it,
+                        contentDescription = null,
+                        modifier = Modifier
+                            .size(40.dp)
+                            .background(Color.LightGray, CircleShape)
+                            .padding(4.dp)
+                    )
+                }
+            }
+        } else {
+            Row(
+                verticalAlignment = Alignment.CenterVertically,
+                horizontalArrangement = Arrangement.Start
+            ) {
+                Text(
+                    text = name,
+                    color = Color.Gray,
+                    fontSize = 14.sp,
+                    fontWeight = FontWeight.Bold,
+                    modifier = Modifier.padding(end = 4.dp)
+                )
+                Text(
+                    text = timestamp,
+                    color = Color.Gray,
+                    fontSize = 12.sp,
+                    modifier = Modifier.padding(end = 4.dp)
+                )
+            }
+        }
+        Spacer(modifier = Modifier.height(4.dp)) // 유저 정보와 메시지 간의 간격 추가
         Box(
             modifier = Modifier
                 .background(
@@ -126,6 +273,12 @@ fun ChatMessage(message: String, isUserMessage: Boolean) {
             Text(text = message)
         }
     }
+}
+
+
+fun getCurrentTime(): String {
+    val sdf = SimpleDateFormat("hh:mm a", Locale.getDefault())
+    return sdf.format(Date())
 }
 
 fun getResponse(message: String): String {
--- app/src/main/java/com/kks/proj_i_happy/ui/screens/SearchScreen.kt
+++ app/src/main/java/com/kks/proj_i_happy/ui/screens/SearchScreen.kt
@@ -1,18 +1,161 @@
 package com.kks.proj_i_happy.ui.screens
 
+import androidx.compose.foundation.background
+import androidx.compose.foundation.border
+import androidx.compose.foundation.layout.Arrangement
+import androidx.compose.foundation.layout.Column
+import androidx.compose.foundation.layout.ExperimentalLayoutApi
+import androidx.compose.foundation.layout.FlowRow
+import androidx.compose.foundation.layout.Row
+import androidx.compose.foundation.layout.Spacer
+import androidx.compose.foundation.layout.fillMaxWidth
+import androidx.compose.foundation.layout.padding
+import androidx.compose.foundation.layout.size
+import androidx.compose.foundation.shape.CircleShape
+import androidx.compose.foundation.shape.RoundedCornerShape
+import androidx.compose.material.icons.Icons
+import androidx.compose.material.icons.filled.Search
+import androidx.compose.material3.ExperimentalMaterial3Api
+import androidx.compose.material3.Icon
+import androidx.compose.material3.IconButton
 import androidx.compose.material3.Text
+import androidx.compose.material3.TextField
+import androidx.compose.material3.TextFieldDefaults
 import androidx.compose.runtime.Composable
+import androidx.compose.runtime.mutableStateOf
+import androidx.compose.runtime.remember
+import androidx.compose.ui.Alignment
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.graphics.Color
+import androidx.compose.ui.res.painterResource
+import androidx.compose.ui.text.font.FontWeight
+import androidx.compose.ui.text.input.TextFieldValue
+import androidx.compose.ui.unit.dp
+import androidx.compose.ui.unit.sp
 import androidx.navigation.NavController
+import com.kks.proj_i_happy.R
 import com.kks.proj_i_happy.util.DefaultScreen
+import com.kks.proj_i_happy.util.HorizonWideButton
 
 @Composable
 fun SearchScreen(navController: NavController) {
-    DefaultScreen(navController) {
+    DefaultScreen(navController = navController) {
         SearchBody()
     }
 }
 
+@OptIn(ExperimentalMaterial3Api::class, ExperimentalLayoutApi::class)
 @Composable
-fun SearchBody(){
-    Text(text = "Search Screen")
+fun SearchBody() {
+    val searchQuery = remember { mutableStateOf(TextFieldValue("")) }
+    val popularSearches = listOf(
+        "아이가 읽기 좋은 책", "수면관리", "학교", "아이를 위한 습관", "수유 팁",
+        "기침이 잦을 때", "초등학교 입학 준비물"
+    )
+    val categories = listOf("건강관리", "육아", "놀이", "자기계발")
+
+    Column(
+        modifier = Modifier
+            .padding(16.dp)
+            .fillMaxWidth(),
+        verticalArrangement = Arrangement.spacedBy(16.dp)
+    ) {
+        // 검색 바
+        Row(
+            modifier = Modifier
+                .fillMaxWidth()
+                .background(Color(0xFFF0F0F0), shape = CircleShape)
+                .padding(horizontal = 16.dp, vertical = 8.dp),
+            verticalAlignment = Alignment.CenterVertically
+        ) {
+            TextField(
+                value = searchQuery.value,
+                onValueChange = { searchQuery.value = it },
+                placeholder = { Text("필요한 정보를 검색하세요") },
+                colors = TextFieldDefaults.textFieldColors(
+                    containerColor = Color.Transparent,
+                    focusedIndicatorColor = Color.Transparent,
+                    unfocusedIndicatorColor = Color.Transparent
+                ),
+                modifier = Modifier.weight(1f)
+            )
+            IconButton(onClick = { /* 검색 기능 구현 */ }) {
+                Icon(Icons.Filled.Search, contentDescription = "Search")
+            }
+        }
+
+        // 인기 검색어
+        Column(
+            verticalArrangement = Arrangement.spacedBy(8.dp)
+        ) {
+            Text(
+                "popular searches",
+                fontSize = 16.sp,
+                color = Color.Gray,
+                modifier = Modifier.padding(vertical = 10.dp)
+            )
+            FlowRow(
+                modifier = Modifier.fillMaxWidth(),
+                horizontalArrangement = Arrangement.spacedBy(8.dp),
+                verticalArrangement = Arrangement.spacedBy(8.dp)
+            ) {
+                popularSearches.forEach { search ->
+                    Text(
+                        text = search,
+                        modifier = Modifier
+                            .background(Color.White, shape = RoundedCornerShape(20.dp))
+                            .border(1.2.dp, Color.Black, shape = RoundedCornerShape(20.dp))
+                            .padding(horizontal = 18.dp, vertical = 9.dp),
+                        color = Color.Black,
+                        fontSize = 14.sp,
+                        fontWeight = FontWeight.Bold
+                    )
+                }
+            }
+        }
+
+        // 카테고리
+        Column(
+            verticalArrangement = Arrangement.spacedBy(8.dp)
+        ) {
+            Text(
+                "Categories", fontSize = 16.sp, color = Color.Gray,
+                modifier = Modifier.padding(vertical = 10.dp)
+            )
+            Column(
+                verticalArrangement = Arrangement.spacedBy(40.dp)
+            ) {
+                categories.forEach { category ->
+                    Row(
+                        verticalAlignment = Alignment.CenterVertically,
+                    ) {
+                        Icon(
+                            painter = painterResource(id = R.drawable.heart),
+                            contentDescription = null,
+                            modifier = Modifier.size(24.dp),
+                            tint = Color.Red
+                        )
+                        Text(
+                            text = category,
+                            modifier = Modifier.padding(start = 8.dp),
+                            fontSize = 16.sp
+                        )
+                    }
+                }
+            }
+        }
+
+        Spacer(modifier = Modifier.size(20.dp))
+
+        // 검색 버튼
+        HorizonWideButton(
+            colors = listOf(Color.Red, Color.White),
+            text = "검색",
+            border = false,
+            modifier = Modifier.fillMaxWidth()
+        ) {
+            /* 검색 기능 구현 */
+        }
+
+    }
 }
